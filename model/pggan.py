a4551e626a68dad7cc30afbb7dae6e9e2b6af669
JermmyXu <503262037@qq.com>
2018-11-18 18:20:29 +0800

    add base generator

--- model/pggan.py
+++ model/pggan.py
@@ -4,3 +4,15 @@ from torch.nn.parameter import Parameter
 
 from .base_model import PixelNormLayer, LayerNormLayer
 
+
+class Generator(nn.Module):
+
+    def __init__(self, target_resolution=1024):
+        super(Generator, self).__init__()
+
+
+    def load_model(self, model_file):
+        self.load_state_dict(torch.load(model_file))
+
+    def save_model(self, model_file):
+        torch.save(self.state_dict(), model_file)
